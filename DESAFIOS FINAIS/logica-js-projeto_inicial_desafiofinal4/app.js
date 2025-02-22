053e85de826ca0737506f66eb2e670ce46dea15c
Gabriel Holanda <139644915+DutchRagazzo@users.noreply.github.com>
2025-02-22 15:51:59 -0300

    Add files via upload

---
+++ DESAFIOS FINAIS/logica-js-projeto_inicial_desafiofinal4/app.js
@@ -0,0 +1,3 @@
+//Utilize o prompt e faça a seguinte pergunta: Qual a linguagem de programação que você mais gosta?. Em seguida, armazene a resposta em uma variável e mostre no console do navegador.
+let preferlang = prompt('Qual linguagem de Programação você mais gosta?')
+console.log(preferlang);
---
+++ DESAFIOS FINAIS/logica-js-projeto_inicial_desafiofinal8/app.js
@@ -0,0 +1,9 @@
+//Crie uma variável "numero" e peça um valor com prompt verifique se é positivo, negativo ou zero. Use if-else para imprimir a respectiva mensagem.
+let numero = prompt('Digite um número: ');
+if (numero>0){
+    alert(`O número ${numero} é positivo`);
+}else if(numero<0){
+    alert(`O número ${numero} é negativo`);
+}else{
+    alert(`O número ${numero} é zero`)
+}
